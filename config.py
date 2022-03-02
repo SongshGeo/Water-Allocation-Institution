4759fcd6bf8a6c6832366971596b49d9a62db753
Shuang Song <songshgeo@gmail.com>
2022-03-02 20:28:42 +0800

    feat(initialize): init repo

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+# Created date: 2022/3/2
+# @Author  : Shuang Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Research Gate: https://www.researchgate.net/profile/Song_Shuang9
+
+
+ROOT = "/Users/songshgeo/Documents/Pycharm/WAInstitution_YRB_2021"
